1a68f037fbbe609c794d1fbfe32326c288ec4e20
Developer sam <gitongas684@gmail.com>
2023-05-02 15:54:24 +0300

    w

--- src/main/java/com/devSam/rentalapplication/Controller/UserController.java
+++ src/main/java/com/devSam/rentalapplication/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.devSam.rentalapplication.Controller;
 import com.devSam.rentalapplication.Entity.User;
 import com.devSam.rentalapplication.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserService userService;
+    @PostMapping("register/user")
     private User regiterUser(@RequestBody User user){
         return userService.regsterUser(user);
     }

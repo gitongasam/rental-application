73b1616a66ad32a6e28a1716c483502eaca8e316
Developer sam <gitongas684@gmail.com>
2023-05-04 07:26:31 +0300

    3

--- src/main/java/com/devSam/rentalapplication/Entity/User.java
+++ src/main/java/com/devSam/rentalapplication/Entity/User.java
@@ -1,6 +1,9 @@
 package com.devSam.rentalapplication.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +13,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long nationalId;
     private String firstName;
     private String lastName;
